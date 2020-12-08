1c09cf3cdda2679c81712a274f7f635b6fef2bd8
Minguinho99 <jyjeon99@kaist.ac.kr>
2020-12-08 15:11:40 +0900

    knapsack mutant

---
+++ Mutants/knapsack/knapsack16.py
@@ -0,0 +1,20 @@
+def knapsack(capacity, items):
+    from collections import defaultdict
+    memo = defaultdict(int)
+
+    for i in range(1, len(items) + 1):
+        if (i == len(items))
+            weight, value = items[0]
+        else
+            weight, value = items[i]
+
+        for j in range(1, capacity + 1):
+            memo[i, j] = memo[i - 1, j]
+
+            if weight <= j:
+                memo[i, j] = max(
+                    memo[i, j],
+                    value + memo[i - 1, j - weight]
+                )
+
+    return memo[len(items), capacity]
