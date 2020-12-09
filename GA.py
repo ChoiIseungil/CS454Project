007e3c61e05acfd42d91b55a11ba2f3be3cdf101
DonghwanKIM0101 <kdoh2522@kaist.ac.kr>
2020-12-09 22:32:46 +0900

    12.09

--- GA.py
+++ GA.py
@@ -9,12 +9,10 @@ def fitness(sequence, fun_name):
     mutation_score = get_mutation_score(fun_name, sequence)
     return mutation_score
 
-def exp(x):
-    return e ** x
 
 def softmax(lst):
     result = []
-    lst = list(map(exp, lst))
+    lst = list(map(math.exp, lst))
     sigma = sum(lst)
     for value in lst:
         result.append(value/sigma)
